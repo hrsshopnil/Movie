658314616677d1caa3b53ebd3a6d8d3c0393ee3e
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-11-07 00:46:10 +0600

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by shopnil hasan on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
